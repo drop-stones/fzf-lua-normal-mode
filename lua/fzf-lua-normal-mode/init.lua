1346a84d83b22d27024faa98b66a05cf6091649b
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-04-09 23:16:58 +0900

    feat(keys): Register keymaps with user-provided keys

--- lua/fzf-lua-normal-mode/init.lua
+++ lua/fzf-lua-normal-mode/init.lua
@@ -1,4 +1,5 @@
 local config = require("fzf-lua-normal-mode.config")
+local keys = require("fzf-lua-normal-mode.keys")
 
 local M = {}
 
@@ -6,6 +7,7 @@ local M = {}
 ---@param user_options? FzfLuaNormalModeOptions
 function M.setup(user_options)
   local _keys, _defer_ms = config.setup(user_options or {})
+  keys.setup_keymap(_keys, _defer_ms)
 end
 
 return M
--- lua/fzf-lua-normal-mode/keys.lua
+++ lua/fzf-lua-normal-mode/keys.lua
@@ -1,5 +1,33 @@
+local bindings = require("fzf-lua-normal-mode.bindings")
+
 ---@class FzfLuaNormalModeKey
 ---@field key string
 ---@field action string|fun()
 ---@field repeatable? boolean
 ---@field wait_user_input? boolean
+
+local M = {}
+
+---Register keymaps for fzf-lua buffer based on key type.
+---@param keys FzfLuaNormalModeKey[]
+---@param defer_ms integer
+function M.setup_keymap(keys, defer_ms)
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "fzf",
+    callback = function()
+      for _, keybind in ipairs(keys) do
+        if type(keybind.action) == "function" then
+          vim.keymap.set("n", keybind.key, keybind.action, { buffer = true })
+        elseif keybind.wait_user_input == true then
+          bindings.wait_user_input(keybind.key, keybind.action --[[@as string]], defer_ms)
+        elseif keybind.repeatable == true then
+          bindings.repeatable(keybind.key, keybind.action --[[@as string]], defer_ms)
+        else
+          bindings.insert_once(keybind.key, keybind.action --[[@as string]])
+        end
+      end
+    end,
+  })
+end
+
+return M
